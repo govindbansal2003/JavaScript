b986e63d1338147a21efeb2fcf33953406c44abe
Govind Bansal <bansalgovind2003@gmail.com>
2025-12-14 18:41:24 +0530

    basics

---
+++ basics/variables.js
@@ -0,0 +1,14 @@
+const accountId = 123456; // cannot be changed (final in java)
+let accountEmail = "fdfdfdf";  
+var accountPassword = "pa$$w0rd"; // dont use this ever scope issue(function scoped)
+accounntCity = "New York"; // this is vague but possible
+
+let accountState;
+
+
+// accountId = 654321; 
+// console.log(accountId);// error
+
+console.table([accountId, accountEmail, accountPassword, accounntCity, accountState]);
+
+
